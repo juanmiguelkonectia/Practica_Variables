d2e0fe3f5a237e0e5eadbff0449dc4642aef6cf0
JMKonectia <juanmiguelkonectia@gmail.com>
2026-01-16 13:38:25 +0100

    La he liado
    Confundi repositorio, pero creo que ya esta bien

---
+++ js/01_variables.js
@@ -0,0 +1,22 @@
+let nombre ; /*declaro una variable llamana nombre de contenido null o vacio*/
+nombre = "Juanmi"; /* Inicializo la variable y asigno el valor "Juanmi" a la variable name*/
+
+let apellido="García"; /*declaro y asigno el valor "García" a la variable apellido*/
+
+let dinero=false; /*declaro y asigno el valor booleano false a la variable dinero*/
+
+let nombre_completo= "Curro Jiménez"; /*declaro y asigno el valor "Curro Jiménez" a la variable nombre_completo*/
+
+const gravedad = 9.81; /*declaro y asigno el valor 9.81 a la constante gravedad*/
+
+/*Var es otra forma de asignar variables pero esta en desuso y no se recomienda su uso*/
+
+var surname = "López"; /*declaro y asigno el valor "López" a la variable surname*/
+
+nombre = "Pedro"; /*reasigno el valor "Pedro" a la variable nombre*/
+
+/*gravedad = 10; /*esto generará un error porque las constantes no pueden ser reasignadas*/
+
+nombre_completo=nombre + " " + apellido;
+
+console.log(nombre_completo); /*imprimo en consola el valor de la variable nombre_completo*/
